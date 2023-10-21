4dc957cdee1e91c5e0f09ba737d1f36bbd2d5f01
lirikape <lirikape@gmail.com>
2023-10-21 10:24:05 +0300

    hw-02-phonebook

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,67 @@
+import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
+import React from 'react';
+import styles from './ContactForm.module.css';
+
+export class ContactForm extends React.Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChangeInput = ({ target }) => {
+    const {name, value} = target;
+
+    if (name === 'number' && !/^[0-9]*$/.test(value)) {
+        // Перевірка, чи введене значення містить лише цифри
+        return;
+      }
+
+    this.setState({ [name]: value });
+    
+  };
+  
+  handleOnClick = () => {
+    const {name, number} = this.state
+    if (name.trim() === '' || number.trim() === '') {
+        return
+      }
+
+    this.props.onAddNewUser({id: nanoid, name, number})
+    this.setState({ name: '', number: '' });
+  }
+
+  render() {
+    const {name, number} = this.state
+    return (
+      
+      <div className={styles.inputСontainer}>
+        <h2>Name</h2>
+        <input
+        //   type="text"
+        onChange={this.handleChangeInput}
+          name="name"
+          value={name}
+          required
+        />
+        <h2>Number</h2>
+        <input 
+        className={styles.inputForm}
+        // type="tel" 
+        name="number" 
+        onChange={this.handleChangeInput}
+        value={number} 
+        pattern="[0-9]*"
+        required />
+        
+        <hr></hr>
+        <button onClick={this.handleOnClick} className={styles.addButton}>Add contact</button>
+        </div>
+    );
+  }
+
+}
+
+ContactForm.propTypes = {
+  onAddNewUser: PropTypes.func.isRequired,
+};
